779f85367734a9a9a31e403a6db7f465af45324c
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2024-10-08 18:49:41 +0530

    Update ashish.js
    add the linked list program in the code
    
    Co-Authored-By: madhu8380 <182326348+madhu8380@users.noreply.github.com>

--- ashish.js
+++ ashish.js
@@ -1,1 +1,50 @@
-ashish
+class Node {
+    constructor(data) {
+        this.data = data;   // Store the data
+        this.next = null;   // Initialize the next pointer to null
+    }
+}
+
+
+class LinkedList {
+    constructor() {
+        this.head = null;  // The head node (starting point) of the list
+    }
+
+    // Method to add a node at the end of the list
+    append(data) {
+        const newNode = new Node(data);
+
+        // If the list is empty, make the new node the head
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            // Traverse to the end of the list
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            // Add the new node at the end
+            current.next = newNode;
+        }
+    }
+
+    // Method to print the list
+    printList() {
+        let current = this.head;
+        while (current !== null) {
+            console.log(current.data);  // Print the data of each node
+            current = current.next;     // Move to the next node
+        }
+    }
+}
+const list = new LinkedList();
+
+// Append some nodes to the list
+list.append(10);
+list.append(20);
+list.append(30);
+
+// Print the list
+list.printList();  // Output: 10 20 30
+
